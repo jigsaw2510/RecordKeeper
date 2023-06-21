e52f969c0ff8e5c7859d132c37a6dbfadf7e5a77
Catalin Gavrila <cata_bb92@yahoo.com>
2023-06-21 22:40:05 +0300

    implemented binding

--- app/src/main/java/com/example/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/example/recordkeeper/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.recordkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recordkeeper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun checkBinding() {
+
+        binding.frameContent
+        binding.bottomNav
+
     }
 }
